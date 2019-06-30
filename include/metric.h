a6d12aade097722faae5f1c86b82f241007b73a4
Jens Korinth <jkorinth@gmx.net>
2019-06-30 17:41:46 +0200

    Implement basic conversions in distance_cast

--- include/metric.h
+++ include/metric.h
@@ -2,24 +2,117 @@
 #define METRIC_METRIC_H_
 
 #include <ratio>
+#include <type_traits>
 
 namespace metric {
 
+
+/* @{ Base Types */
 template <typename Repr, typename Ratio = std::ratio<1>>
 struct distance {
-  explicit constexpr distance(Repr c) noexcept : count(c) {}
-  const Repr count;
-  operator Repr() const noexcept { return count; }
+  using repr = Repr;
+  using ratio = Ratio;
+  constexpr distance() noexcept = default;
+
+  distance(const distance&) = default;
+
+  template <class Repr2, typename = std::enable_if_t<std::is_convertible_v<Repr, Repr2>>>
+  constexpr explicit distance(const Repr2& r) : distance(r) {};
+
+  template <class Repr2, class Ratio2>
+  constexpr distance(const distance<Repr2, Ratio2>& d);
+
+  explicit constexpr distance(Repr c) noexcept : count_(c) {}
+
+  // operator Repr() const noexcept { return count; }
+  inline Repr count() const noexcept { return count_; }
+
+  const Repr count_;
 };
 
-template <typename Repr> using nanometers = distance<Repr, std::nano>;
+template <typename Repr> using nanometers  = distance<Repr, std::nano>;
 template <typename Repr> using micrometers = distance<Repr, std::micro>;
 template <typename Repr> using millimeters = distance<Repr, std::milli>;
 template <typename Repr> using centimeters = distance<Repr, std::centi>;
-template <typename Repr> using decimeters = distance<Repr, std::deci>;
-template <typename Repr> using meters = distance<Repr>;
-template <typename Repr> using kilometers = distance<Repr, std::kilo>;
-template <typename Repr> using megameters = distance<Repr, std::mega>;
+template <typename Repr> using decimeters  = distance<Repr, std::deci>;
+template <typename Repr> using meters      = distance<Repr>;
+template <typename Repr> using kilometers  = distance<Repr, std::kilo>;
+template <typename Repr> using megameters  = distance<Repr, std::mega>;
+/* Base Types @} */
+
+/* @{ Arithmetic Operators */
+/* Arithmetic Operators @} */
+
+/* @{ Operators */
+template <typename T>
+struct is_distance : std::false_type {};
+
+template <typename Repr, typename Ratio>
+struct is_distance<distance<Repr, Ratio>> : std::true_type {};
+
+template <typename Repr, typename Ratio>
+struct is_distance<const distance<Repr, Ratio>> : std::true_type {};
+
+template <typename Repr, typename Ratio>
+struct is_distance<volatile distance<Repr, Ratio>> : std::true_type {};
+
+template <typename Repr, typename Ratio>
+struct is_distance<const volatile distance<Repr, Ratio>> : std::true_type {};
+
+}  // namespace chrono
+
+template <typename Repr1, typename Ratio1, typename Repr2, typename Ratio2>
+struct std::common_type<metric::distance<Repr1, Ratio1>, metric::distance<Repr2, Ratio2>> {
+  using type = metric::distance<typename std::common_type<Repr1, Repr2>::type,
+                                typename __ratio_gcd<Ratio1, Ratio2>::type>;
+};
+
+namespace metric {
+
+template <typename FromDistance, typename ToDistance,
+          typename Ratio = typename std::ratio_divide<
+            typename FromDistance::ratio,
+            typename ToDistance::ratio
+          >::type,
+          bool = Ratio::num == 1,
+          bool = Ratio::den == 1>
+struct __distance_cast;
+
+template <class FromDistance, class ToDistance, class Ratio>
+struct __distance_cast<FromDistance, ToDistance, Ratio, true, true> {
+  inline constexpr ToDistance operator()(const FromDistance& fd) const {
+    return ToDistance(static_cast<typename ToDistance::repr>(fd.count()));
+  }
+};
+
+template <class FromDistance, class ToDistance, class Ratio>
+struct __distance_cast<FromDistance, ToDistance, Ratio, true, false> {
+  inline constexpr ToDistance operator()(const FromDistance& fd) const {
+    using CT = typename std::common_type<typename ToDistance::repr,
+                                         typename FromDistance::repr,
+                                         intmax_t>::type;
+    return ToDistance(static_cast<typename ToDistance::repr>(
+          static_cast<CT>(fd.count()) / static_cast<CT>(Ratio::den)));
+  }
+};
+
+template <class FromDistance, class ToDistance, class Ratio>
+struct __distance_cast<FromDistance, ToDistance, Ratio, false, true> {
+  inline constexpr ToDistance operator()(const FromDistance& fd) const {
+    using CT = typename std::common_type<typename ToDistance::repr,
+                                         typename FromDistance::repr,
+                                         intmax_t>::type;
+    return ToDistance(static_cast<typename ToDistance::repr>(
+          static_cast<CT>(fd.count()) * static_cast<CT>(Ratio::num)));
+  }
+};
+
+template <class ToDistance, class Repr, class Ratio>
+constexpr std::enable_if_t<is_distance<ToDistance>::value, ToDistance>
+distance_cast(const distance<Repr, Ratio>& d) {
+  return __distance_cast<distance<Repr, Ratio>, ToDistance>()(d);
+}
+/* Operators @} */
 
 namespace literals {
 using ull = unsigned long long;
--- test/test_metric.cpp
+++ test/test_metric.cpp
@@ -4,7 +4,18 @@
 using namespace metric;
 using namespace metric::literals;
 
-TEST(MetricTest, nm_operator_ull) {
-  auto d { 5_nm };
-  EXPECT_EQ(5, d);
+TEST(MetricTest, distance_cast_conversions) {
+  auto d { 5.0_m };
+  auto nm { distance_cast<nanometers<decltype(d)::repr>>(d) };
+  auto um { distance_cast<micrometers<decltype(d)::repr>>(d) };
+  auto mm { distance_cast<millimeters<decltype(d)::repr>>(d) };
+  auto m { distance_cast<meters<decltype(d)::repr>>(d) };
+  auto km { distance_cast<kilometers<decltype(d)::repr>>(d) };
+  auto Mm { distance_cast<megameters<decltype(d)::repr>>(d) };
+  EXPECT_EQ(nm.count(), d.count() * 1000000000);
+  EXPECT_EQ(um.count(), d.count() * 1000000);
+  EXPECT_EQ(mm.count(), d.count() * 1000);
+  EXPECT_EQ(m.count(), d.count());
+  EXPECT_EQ(km.count() * 1000, d.count());
+  EXPECT_EQ(Mm.count() * 1000000, d.count());
 }
